a98b2104d0bb77c057a194a069544c2dd1e99512
karun951 <112156231+karun951@users.noreply.github.com>
2023-04-06 12:30:44 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 class HelloWorld{
 	public static void main(String a[]){
 		System.out.println("Hello World");
-		System.out.println("Hello World");
+		System.out.println("Welcome to MSys Company");
 	}
 }
